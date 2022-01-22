b087557c172f85f936fd01da52d5c811c6f09aae
mohamedtamer0 <mt4452242@gmail.com>
2022-01-22 02:18:08 +0200

    update

--- app/src/main/java/com/example/androidlibrary/MainActivity.kt
+++ app/src/main/java/com/example/androidlibrary/MainActivity.kt
@@ -2,12 +2,32 @@ package com.example.androidlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.toast_preview.ToastPreview
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn1:Button
+    private lateinit var btn2:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+
+
+        btn1.setOnClickListener {
+            ToastPreview.toastShort(this,"Hello Short")
+        }
+
+        btn2.setOnClickListener {
+            ToastPreview.toastLong(this,"Hello Long")
+        }
+
 
 
 
--- toast-preview/src/main/java/com/example/toast_preview/ToastPreview.kt
+++ toast-preview/src/main/java/com/example/toast_preview/ToastPreview.kt
@@ -3,11 +3,22 @@ package com.example.toast_preview
 import android.content.Context
 import android.widget.Toast
 
+
+
 class ToastPreview {
+
     companion object {
 
-        fun test(context: Context, msg: String) {
+        
+        fun toastShort(context: Context, msg: String) {
             Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
         }
+
+        fun toastLong(context: Context, msg: String) {
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
+
